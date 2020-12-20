00ed0179960d85b070b499ed2dde257306f5d9b5
han1202012 <octopus_truth@163.com>
2020-12-20 14:47:24 +0800

    update

--- entry/src/main/java/com/example/harmony/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/harmony/slice/MainAbilitySlice.java
@@ -3,12 +3,35 @@ package com.example.harmony.slice;
 import com.example.harmony.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+import ohos.agp.components.Component;
+import ohos.agp.components.Text;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+
+        // 获取 XML 布局中的 Button 按钮
+        Button button = (Button) findComponentById(ResourceTable.Id_button);
+
+        // 设置 Button 按钮点击事件
+        button.setClickedListener(new Component.ClickedListener() {
+            @Override
+            public void onClick(Component component) {
+                // 初始化要跳转的界面
+                AbilitySlice slice = new MainAbilitySlice2();
+
+                // 初始化传递的 Intent 意图
+                Intent intent = new Intent();
+                intent.setParam("DATA", 888);
+
+                // 跳转到 MainAbilitySlice2 界面
+                present(slice, intent);
+            }
+        });
+
     }
 
     @Override
