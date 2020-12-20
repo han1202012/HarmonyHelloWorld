00216a3441dcf58647744372f61e91cfe0807d10
han1202012 <octopus_truth@163.com>
2020-12-20 16:18:35 +0800

    update

---
+++ ability/src/main/java/com/example/ability/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.ability;
+
+import com.example.ability.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
---
+++ ability/src/test/java/com/example/ability/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.ability;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
