1ff01a2c1d022794b5ce0fe63ea676807d3e0f96
han1202012 <octopus_truth@163.com>
2020-12-24 11:56:55 +0800

    update

--- list/src/main/java/com/example/list/slice/MainAbilitySlice.java
+++ list/src/main/java/com/example/list/slice/MainAbilitySlice.java
@@ -3,9 +3,9 @@ package com.example.list.slice;
 import com.example.list.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
-import ohos.agp.components.Component;
-import ohos.agp.components.ComponentContainer;
-import ohos.agp.components.RecycleItemProvider;
+import ohos.agp.components.*;
+
+import java.util.ArrayList;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
@@ -13,7 +13,17 @@ public class MainAbilitySlice extends AbilitySlice {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
 
+        // 获取布局文件中的 ListContainer 列表
+        ListContainer listContainer = (ListContainer) findComponentById(ResourceTable.Id_listContainer);
 
+        // 构造数据源
+        ArrayList<DataItem> items = new ArrayList<>();
+        items.add(new DataItem("Tom"));
+        items.add(new DataItem("Jerry"));
+        items.add(new DataItem("Trump"));
+
+        // 为列表设置数据源
+        listContainer.setItemProvider(new MyRecycleItemProvider(items, this));
 
     }
 
@@ -28,25 +38,77 @@ public class MainAbilitySlice extends AbilitySlice {
     }
 }
 
+/**
+ * 代表每个列表项数据
+ */
+class DataItem{
+    /**
+     * 列表项字符串
+     */
+    public String text;
+
+    /**
+     * 构造函数初始化列表项
+     * @param text
+     */
+    public DataItem(String text) {
+        this.text = text;
+    }
+}
+
+/**
+ * 数据源
+ */
 class MyRecycleItemProvider extends RecycleItemProvider{
+
+    /**
+     * 数据
+     */
+    private ArrayList<DataItem> items;
+
+    /**
+     * 当前显示列表的界面
+     */
+    private AbilitySlice abilitySlice;
+
+    public MyRecycleItemProvider(ArrayList<DataItem> items, AbilitySlice abilitySlice) {
+        this.items = items;
+        this.abilitySlice = abilitySlice;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return items.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return items.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public Component getComponent(int i, Component component, ComponentContainer componentContainer) {
-        return null;
+        Component ret = null;
+        if (component == null){
+            // 创建当前文本组件
+            Text text = new Text(componentContainer.getContext());
+            // 设置宽高
+            text.setWidth(StackLayout.LayoutConfig.MATCH_PARENT);
+            text.setHeight(StackLayout.LayoutConfig.MATCH_CONTENT);
+            // 设置文本
+            text.setText(items.get(i).text);
+            // 设置文本大小
+            text.setTextSize(100);
+            ret = text;
+        }else{
+            ret = component;
+        }
+
+        return ret;
     }
 }
